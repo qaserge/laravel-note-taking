c1b7b395077dd8c7f84e00d203e82acdb8909731
Sergei <qaserge@gmail.com>
2023-09-10 13:35:46 -0400

    notes/index.blade.php added with all notes from the controller

---
+++ resources/views/notes/index.blade.php
@@ -0,0 +1,26 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Notes') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{-- {{ __("All Notes") }} // translate example --}}
+                    @forelse ($notes as $note)
+                        <div class="my-6 p-6 border-b border-gray-200">
+                            <h2 class="font-bold text-2xl">{{ $note->title }}</h2>
+                            <p class="mt-2">{{ $note->text }}</p>
+                            <span class="block mt-4 text-sm opacity-70">{{ $note->updated_at->diffForHumans() }}</span>
+                        </div>
+                    @empty
+                        <p>You have no notes yet.</p>
+                    @endforelse
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
